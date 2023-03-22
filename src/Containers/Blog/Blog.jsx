36e0ad2b3ddaa42a9013eecdb344af6e5987695c
natherma <65506524+natherma@users.noreply.github.com>
2023-03-22 20:55:12 +0530

    Just two section left

--- src/Containers/Blog/Blog.jsx
+++ src/Containers/Blog/Blog.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import './blog.css'
+import blog01 from '../../assets/blog01.png'
+import blog02 from '../../assets/blog02.png'
+import blog03 from '../../assets/blog03.png'
+import blog04 from '../../assets/blog04.png'
+import blog05 from '../../assets/blog05.png'
+import Article from '../../components/article/Article'
 
 export default function Blog() {
   return (
-    <div>Blog</div>
+    <div className='gpt__blog section_padding'>
+      <div className='gpt__blog heading'>
+      <h1 className="gradient_text">A lot is happening, <br /> We are blogging about it.</h1>
+      </div>
+      <div className='gpt__blog-container'>
+      <div className="gpt3__blog-container_groupA">
+        <Article imgUrl={blog01} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+      </div>
+      <div className="gpt3__blog-container_groupB">
+        <Article imgUrl={blog02} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog03} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog04} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog05} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+      </div>
+      </div>
+    </div>
   )
 }
--- src/components/Call_to_aaction/Cat.jsx
+++ src/components/Call_to_aaction/Cat.jsx
@@ -3,6 +3,16 @@ import './cta.css'
 
 export default function Cat() {
   return (
-    <div>Cat</div>
+    <div className='gpt__cat'>
+      <div className='gpt__cat-content'>
+      <p>Request Early Access to Get Started</p>
+      <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className='gpt__cat-btn'>
+        <button type='button'>
+          Get Started
+        </button>
+      </div>
+    </div>
   )
 }
